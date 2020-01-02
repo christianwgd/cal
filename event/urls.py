e77f3087d66286bfb07fdaa965c570d71cb97f61
Christian Wiegand <cwiegand@wgdnet.de>
2020-01-02 10:46:23 +0100

    Ical sync

--- event/urls.py
+++ event/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('list/', views.EventListView.as_view(), name='list'),
     path('json/', views.events_as_json, name='as_json'),
     path('json/<str:calendar>/<str:location>/', views.events_as_json, name='as_json'),
+    path('ical/<str:cal_slug>/', views.sync_ical, name='as_ical'),
+    path('ical/<str:cal_slug>/<str:location>/', views.sync_ical, name='as_ical'),
 ]
--- event/views.py
+++ event/views.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 import json
 import dateutil.parser
+import datetime
 from django.utils import formats
 
 from django.http import HttpResponse
 from django.views.generic import ListView
+from icalendar import Alarm, vText
+from icalendar import Calendar as iCalendar
+from icalendar import Event as icalEvent
 
-from event.models import Event, Location, CONTENT_STATUS_PUBLISHED
+from event.models import Event, Location, Calendar, CONTENT_STATUS_PUBLISHED
 
 
 class EventListView(ListView):
@@ -19,8 +23,6 @@ class EventListView(ListView):
 
 
 def events_as_json(request, calendar=None, location=None):
-
-    print(calendar, location)
 
     start_str = request.GET['start']
     end_str = request.GET['end']
@@ -52,3 +54,59 @@ def events_as_json(request, calendar=None, location=None):
         cal.append(cal_item)
 
     return HttpResponse(json.dumps(cal))
+
+
+def sync_ical(request, cal_slug, location=None):
+
+    calendar = Calendar.objects.get(slug=cal_slug)
+
+    now = datetime.datetime.now()
+    if location:
+        events = Event.objects.filter(
+            calendar=calendar,
+            location__slug=location,
+            date__gte=now,
+            state=CONTENT_STATUS_PUBLISHED
+        ).order_by('date')
+    else:
+        events = Event.objects.filter(
+            calendar__slug=calendar,
+            date__gte=now,
+            state=CONTENT_STATUS_PUBLISHED
+        ).order_by('date')
+
+    ical = iCalendar()
+    ical.add('version', '2.0')
+    ical.add('prodid', '-//%s//NONSGML Event Calendar//EN' % calendar.slug.upper())
+    ical.add('X-WR-CALNAME', calendar.name)
+    ical.add('X-WR-TIMEZONE', 'Europe/Berlin')
+    ical.add('method', 'PUBLISH')
+
+    for event in events:
+        cal_event = icalEvent()
+        cal_event.add('uid', '%s-SNDN-0000-0000-%012d' % (calendar.slug.upper(), event.id))
+        cal_event.add('created', now)
+        cal_event.add('dtstart', event.date)
+
+        cal_event.add('summary', event.category.name)
+
+        cal_event.add('location', event.location.name)
+
+        cal_event.add('dtstamp', now)
+        cal_event.add('sequence', '%d' % event.version)
+
+        alarm = Alarm()
+        alarm.add('trigger', vText('-PT16H'))
+        alarm.add('action', 'DISPLAY')
+        alarm.add('description', '{calname}: {catname}'.format(
+            calname = event.calendar.name,
+            catname = event.category.name
+        ))
+        cal_event.add_component(alarm)
+
+        ical.add_component(cal_event)
+    # end for
+
+    response = HttpResponse(ical.to_ical(), content_type="text/calendar, text/x-vcalendar, application/hbs-vcs")
+    response['Content-Disposition'] = 'attachment; filename="{}.ics"'.format(calendar.slug)
+    return response
